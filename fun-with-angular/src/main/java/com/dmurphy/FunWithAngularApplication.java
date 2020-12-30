cc15eb18574a84077e3d84c1ab0dd90d801791b5
DMurphy <DMurphy@10.1.10.182>
2020-12-29 23:14:02 -0500

    initial commit, bare bones of angular front end and java back end project.

---
+++ fun-with-angular/src/main/java/com/dmurphy/FunWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.dmurphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunWithAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunWithAngularApplication.class, args);
+	}
+
+}
